03eb2a7a6bdc635ee3d2a98e880ca21eaf39e52b
Diego González Liarte <diegogliarte@gmail.com>
2026-01-24 22:39:32 +0100

    add utm link

--- src/lib/config/links.ts
+++ src/lib/config/links.ts
@@ -1,6 +1,6 @@
 export const LINKS = {
 	vpetlings: {
-		steam: 'https://store.steampowered.com/app/3799160/VPetlings/',
+		steam: 'https://store.steampowered.com/app/3799160/VPetlings/?utm_source=sargantanagames&utm_medium=web&utm_campaign=website&utm_content=hyperlinks',
 		twitter: 'https://x.com/sargantanagames',
 		instagram: 'https://www.instagram.com/sargantanagames/'
 	}
